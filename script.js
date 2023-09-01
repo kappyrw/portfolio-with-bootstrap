3319448a92491ec8bdfae1367bfe62b4e4ee0937
kappyrw <93374495+kappyrw@users.noreply.github.com>
2023-09-01 16:13:31 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,26 @@
 
-function formValidation()
-{
-  var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    var name=document.form.Name.value;
-    var email=document.form.Email.value;
-    var Message= document.form.Message.value;
+  function formValidation() {
+    // Get form elements
+    var name = document.forms["contactForm"]["Name"].value;
+    var email = document.forms["contactForm"]["Email"].value;
+    var message = document.forms["contactForm"]["Message"].value;
 
-    if(email="" || !emailRegex.test(email))
-       {
-         alert("please enter a valid email");
-         email.focus();
-         return false;
-       }
-    if(message.lenght<10 || messaage.lenght>500 )
-       {
-         alert("Message should be atleast 10 characters and 500 characters maximum");
-         message.focus();
-         return false;
-       }
-}
-   
+    // Regular expression for email validation
+    var emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+    // Check if name, email, and message are not empty
+    if (name === "" || email === "" || message === "") {
+      alert("Please fill in all fields.");
+      return false;
+    }
+
+    // Check if the email is valid using regex
+    if (!email.match(emailPattern)) {
+      alert("Please enter a valid email address.");
+      return false;
+    }
+
+    // If all validations pass, the form will submit
+    return true;
+  }
 
