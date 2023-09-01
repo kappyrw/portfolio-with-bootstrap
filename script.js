807fcfc46d792dd5c868306c8f313285358505d7
kappyrw <93374495+kappyrw@users.noreply.github.com>
2023-09-01 16:04:59 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,54 +1,23 @@
 
 function formValidation()
 {
-document.addEventListener("DOMContentLoaded", function () {
-    const form = document.querySelector("form");
-    const nameInput = document.querySelector("input[name='name']");
-    const emailInput = document.querySelector("input[name='emeil']"); // Typo in the HTML attribute, should be 'email'
-    const messageInput = document.querySelector("textarea[name='msg']");
-    const sendButton = document.getElementById("send");
-    const globalError = document.getElementById("globalError");
+  var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    var name=document.form.Name.value;
+    var email=document.form.Email.value;
+    var Message= document.form.Message.value;
 
-    form.addEventListener("submit", function (event) {
-        const name = nameInput.value;
-        const email = emailInput.value;
-        const message = messageInput.value;
-
-        const nameError = document.getElementById("nameError");
-        const emailError = document.getElementById("emailError");
-        const messageError = document.getElementById("messageError");
-
-        nameError.textContent = "";
-        emailError.textContent = "";
-        messageError.textContent = "";
-        globalError.textContent = "";
-
-        if (name.trim() === "") {
-            nameError.textContent = "Name is required";
-            event.preventDefault();
-        }
-
-        if (email.trim() === "") {
-            emailError.textContent = "Email is required";
-            event.preventDefault();
-        } else if (!isValidEmail(email)) {
-            emailError.textContent = "Invalid email format";
-            event.preventDefault();
-        }
-
-        if (message.trim() === "") {
-            messageError.textContent = "Message is required";
-            event.preventDefault();
-        }
-
-        if (name.trim() === "" || email.trim() === "" || message.trim() === "") {
-            globalError.textContent = "Please fill in all required fields.";
-            event.preventDefault();
-        }
-    });
+    if(email="" || !emailRegex.test(email))
+       {
+         alert("please enter a valid email");
+         email.focus();
+         return false;
+       }
+    if(message.lenght<10 || messaage.lenght>500 )
+       {
+         alert("Message should be atleast 10 characters and 500 characters maximum");
+         message.focus();
+         return false;
+       }
 }
-    function isValidEmail(email) {
-        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-        return emailRegex.test(email);
-    }
-});
+   
+
